85aa2b72cc0ff2885d54ce3ea71247babb6bed32
Ankit Yadav <61616007+ankitsmt211@users.noreply.github.com>
2024-12-22 04:26:24 +0530

    update migration and movie model

--- movie/models/movie.py
+++ movie/models/movie.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.db import models
-from django.utils import timezone
 
 from movie.models.genre import Genre
 from movie.models.language import Language
@@ -12,24 +11,25 @@ MOVIE_STATUS = [
     ('In Production', 'In Production'),
     ('Post-Production', 'Post-Production'),
     ('Cancelled', 'Cancelled'),
+    ('NA', 'NA'),
 ]
 
 class Movie(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     budget = models.BigIntegerField(default=0)
-    homepage = models.URLField(default='', null=True, blank=True)
-    original_language = models.TextField(default='')
+    homepage = models.URLField(default='', null=True, blank=True, max_length=500)
+    original_language = models.TextField(default='', null=True, blank=True)
     overview = models.TextField(default='', null=True, blank=True)
-    release_date = models.DateField()
+    release_date = models.DateField(null=True, blank=True)
     revenue = models.BigIntegerField(default=0)
     runtime = models.IntegerField(default=0, null=True, blank=True)
     status = models.CharField(max_length=20, default='Released', choices=MOVIE_STATUS)
-    title = models.CharField(default='', max_length=255)
+    title = models.CharField(default='', max_length=255, unique=True)
     vote_average = models.FloatField(default=0)
     vote_count = models.IntegerField(default=0)
     production_company = models.ForeignKey(Production, on_delete=models.DO_NOTHING)
     genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
-    languages = models.ManyToManyField(Language)
+    languages = models.ManyToManyField(Language, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
